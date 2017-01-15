ffa9dc750ca0af0d77e52ba5ca6c3870725ecaac
Robert430404 <robert@studiorclv.com>
2017-01-14 20:52:47 -0800

    Setup the meetup API call for the meetup events and passed the data to the view

--- app/Http/Controllers/MeetupEventsController.php
+++ app/Http/Controllers/MeetupEventsController.php
@@ -3,16 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use DMS\Service\Meetup\MeetupKeyAuthClient;
 
 class MeetupEventsController extends Controller
 {
-    public function __construct()
-    {
+    /**
+     * @var Redis
+     */
+    protected $redisClient;
 
+    /**
+     * @var MeetupKeyAuthClient
+     */
+    protected $meetupClient;
+
+    public function __construct(Redis $redis, MeetupKeyAuthClient $meetupKeyAuthClient)
+    {
+        $this->redisClient  = $redis;
+        $this->meetupClient = $meetupKeyAuthClient::factory([
+            'key' => getenv('MEETUP_API_KEY'),
+        ]);
     }
 
     public function index()
     {
-        return view('events');
+        $events = $this->meetupClient->getEvents([
+            'group_urlname' => 'PHP-vegas',
+        ])->getData();
+
+        return view('events', [
+            'events' => $events,
+        ]);
     }
 }
