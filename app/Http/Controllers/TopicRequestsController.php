ff47cc967a78dc1463c6fa801671c7ec880b7a19
Robert430404 <robert@studiorclv.com>
2017-01-15 13:48:19 -0800

    Implemted view for the requests page

--- app/Http/Controllers/TopicRequestsController.php
+++ app/Http/Controllers/TopicRequestsController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class TopicRequestsController extends Controller
 {
-    //
+    /**
+     * TopicRequestsController constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+        return view('requests');
+    }
+
+    public function insert(Request $request)
+    {
+
+    }
 }
---
+++ resources/views/requests.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts/app')
+
+@section('content')
+
+    <section class="page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <h3 class="page-title">
+                        Request A Talk Topic
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
