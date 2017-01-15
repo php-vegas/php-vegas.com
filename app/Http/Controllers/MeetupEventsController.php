e717ec1e55ee5ebe0a71a3af5974fb524fec914b
Robert430404 <robert@studiorclv.com>
2017-01-14 20:48:38 -0800

    Styled the inner page headings

--- app/Http/Controllers/MeetupEventsController.php
+++ app/Http/Controllers/MeetupEventsController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MeetupEventsController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+        return view('events');
+    }
 }
---
+++ resources/views/events.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <h3 class="page-title">
+                        Current Meetup Events
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="events-list">
+
+    </section>
+
+@endsection
