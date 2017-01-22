655ba33452f40a6a9c12f9a3dcf10ca7fd8b6c92
Robert430404 <robert@studiorclv.com>
2017-01-22 12:07:01 -0800

    Implemented the proper email format

--- resources/views/emails/topicRequest.blade.php
+++ resources/views/emails/topicRequest.blade.php
@@ -1,1 +1,12 @@
-Email Sent
+A new topic has been requested from the website.
+<br />
+Name: {{ $topic->get('fname') }} {{ $topic->get('lname') }}
+
+<br />
+Email: {{ $topic->get('email') }}
+<br />
+Phone: {{ $topic->get('phone') }}
+<br />
+Reqeust:
+<br />
+{{ $topic->get('request') }}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>Welcome To Fabulous PHP Vegas!</title>
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="/css/app.css" rel="stylesheet">
-
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode([
