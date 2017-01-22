616b692c3338de97ca095833a6a618f1f2dc56cc
Robert430404 <robert@studiorclv.com>
2017-01-22 11:23:44 -0800

    Additional Tweaks to the events page

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -18,31 +18,70 @@
         <div class="container">
             <div class="row events">
                 @foreach($events as $key => $event)
-                    <div class="@if($key === 0) col-lg-12 col-md-12 @else col-lg-6 col-md-6 @endif col-sm-12">
-                        <div class="event">
-                            <div class="heading-meta">
-                                {{ $event['venue']['name'] }} //
-                                {{ $event['venue']['address_1'] }}
-                                {{ $event['venue']['city'] }},
-                                {{ $event['venue']['state'] }}
-                                {{ $event['venue']['zip'] }}
-                            </div>
-                            <h3 class="event-name">
-                                {{ $event['name'] }}
-                            </h3>
-                            {!! $event['description'] !!}
-                            <a href="{{ $event['event_url'] }}" target="_blank" class="learn-more btn btn-primary">
-                                Learn More
-                            </a>
-                            <div class="footer-meta">
-                                Date: {{ date("M d, Y", ($event['time'] / 1000)) }} //
-                                RSVPs: {{ $event['yes_rsvp_count'] }}
+                    @if($key === 0)
+                        <div class=" col-lg-12 col-md-12 col-sm-12">
+                            <div class="event current-event">
+                                <div class="heading-meta">
+                                    {{ $event['venue']['name'] }} //
+                                    {{ $event['venue']['address_1'] }}
+                                    {{ $event['venue']['city'] }},
+                                    {{ $event['venue']['state'] }}
+                                    {{ $event['venue']['zip'] }}
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 col-sm-12">
+                                        <h3 class="event-name">
+                                            {{ $event['name'] }}
+                                        </h3>
+                                        {!! $event['description'] !!}
+                                        <a href="{{ $event['event_url'] }}" target="_blank" class="learn-more btn btn-primary">
+                                            Learn More
+                                        </a>
+                                    </div>
+                                    <div class="col-md-6 col-sm-12">
+                                        <div class="home-event-map"
+                                             id="home-event-map"
+                                             data-lat="{{ $event['venue']['lat'] }}"
+                                             data-lon="{{ $event['venue']['lon'] }}"></div>
+                                    </div>
+                                </div>
+                                <div class="footer-meta">
+                                    Date: {{ date("M d, Y", ($event['time'] / 1000)) }} //
+                                    RSVPs: {{ $event['yes_rsvp_count'] }}
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @else
+                        <div class="col-lg-6 col-md-6 col-sm-12">
+                            <div class="event">
+                                <div class="heading-meta">
+                                    {{ $event['venue']['name'] }} //
+                                    {{ $event['venue']['address_1'] }}
+                                    {{ $event['venue']['city'] }},
+                                    {{ $event['venue']['state'] }}
+                                    {{ $event['venue']['zip'] }}
+                                </div>
+                                <h3 class="event-name">
+                                    {{ $event['name'] }}
+                                </h3>
+                                {!! $event['description'] !!}
+                                <a href="{{ $event['event_url'] }}" target="_blank" class="learn-more btn btn-primary">
+                                    Learn More
+                                </a>
+                                <div class="footer-meta">
+                                    Date: {{ date("M d, Y", ($event['time'] / 1000)) }} //
+                                    RSVPs: {{ $event['yes_rsvp_count'] }}
+                                </div>
+                            </div>
+                        </div>
+                    @endif
                 @endforeach
             </div>
         </div>
     </section>
 
+@endsection
+
+@section('scripts')
+    <script src="https://maps.googleapis.com/maps/api/js?key={{ getenv('MAPS_JS_API_KEY') }}"></script>
 @endsection
