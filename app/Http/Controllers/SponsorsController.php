1d836fb5bc8a02aad6828a34a45707620d550837
Robert430404 <robert@studiorclv.com>
2017-01-22 11:11:47 -0800

    Implemented Sponsors Page

--- app/Http/Controllers/SponsorsController.php
+++ app/Http/Controllers/SponsorsController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use DMS\Service\Meetup\MeetupKeyAuthClient;
 use Illuminate\Http\Request;
 
 class SponsorsController extends Controller
 {
+    protected $meetupClient;
+
+    public function __construct(MeetupKeyAuthClient $meetupKeyAuthClient)
+    {
+        $this->meetupClient = $meetupKeyAuthClient::factory([
+            'key' => getenv('MEETUP_API_KEY'),
+        ]);
+    }
+
     public function index()
     {
-        return view('sponsors');
+        $sponsors = $this->meetupClient->getGroups([
+            'group_urlname' => 'PHP-vegas',
+            'fields'  => 'sponsors'
+        ])->getData()[0]['sponsors'];
+
+        return view('sponsors', [
+            'sponsors' => $sponsors
+        ]);
     }
 }
--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <h3 class="page-title">
-                        About Our Sponsors
+                        Our Sponsors
                     </h3>
                 </div>
             </div>
@@ -15,7 +15,31 @@
     </section>
 
     <section class="sponsors">
-
+        <div class="container">
+            <div class="row">
+                @foreach($sponsors as $sponsor)
+                    <div class="col-md-4 col-sm-12">
+                        <div class="sponsor">
+                            @if(isset($sponsor['image_url']))
+                                <div class="logo-wrapper">
+                                    <img src="{{ $sponsor['image_url'] }}" alt="{{ $sponsor['name'] }}" />
+                                </div>
+                            @else
+                                <div class="logo-wrapper">
+                                    <i class="fa fa-question-circle"></i>
+                                </div>
+                            @endif
+                            <h3 class="sponsor-name">{{ $sponsor['name'] }}</h3>
+                            <p>{{ $sponsor['info'] }}</p>
+                            <hr>
+                            <a href="{{ $sponsor['url'] }}" class="btn btn-primary" target="_blank">
+                                Visit {{ $sponsor['name'] }}
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </section>
 
 @endsection
