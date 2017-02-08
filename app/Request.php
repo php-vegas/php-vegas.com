18b06f1b1b3e7bae6ab47a0adf2cdd29e0907ab9
Robert Cox <rcox@dontbebroke.com>
2017-02-08 14:05:49 -0800

    Setup The model

--- app/Request.php
+++ app/Request.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email_address',
+        'topic_request',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
 }
