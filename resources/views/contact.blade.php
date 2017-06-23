4f7065dcf471b9014c3ad6aa0bc4b466e1670ad3
Robert430404 <robert@studiorclv.com>
2017-06-22 21:54:57 -0700

    Fixed up the contact view and added links for the social profiles

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -22,7 +22,7 @@
                         <i class="fa fa-facebook"></i>
                         <h3 class="headline">Facebook</h3>
                         <p class="tagline">Become part of our group through facebook, and see what we're up to.</p>
-                        <a href="" class="btn btn-primary">
+                        <a href="https://www.facebook.com/PHP-Vegas-1481879648787405/" target="_blank" class="btn btn-primary">
                             Connect With Us
                         </a>
                     </div>
@@ -32,7 +32,7 @@
                         <i class="fa fa-twitter"></i>
                         <h3 class="headline">Twitter</h3>
                         <p class="tagline">Interact with us on twitter for the latest news in the community and the group.</p>
-                        <a href="" class="btn btn-primary">
+                        <a href="https://twitter.com/PHPVegas" target="_blank" class="btn btn-primary">
                             Tweet At Us
                         </a>
                     </div>
@@ -42,8 +42,8 @@
                         <i class="fa fa-slack"></i>
                         <h3 class="headline">Slack Channel</h3>
                         <p class="tagline">Connect with php developers and discuss issues over our community slack channel.</p>
-                        <a href="" class="btn btn-primary">
-                            Talk With Us
+                        <a href="" class="disabled btn btn-primary">
+                            Coming Soon
                         </a>
                     </div>
                 </div>
